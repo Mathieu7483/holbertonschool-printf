c1f43295db86f3602d1b1cbaf5a55e3097d13cdf
Mathieu7483 <11436@holbertonstudents.com>
2025-07-21 10:25:27 +0200

    update main.h

--- main.h
+++ main.h
@@ -1,5 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h> /*pour size_t*/
+#include <stdio.h> /*pour stdout du printf*/
+#include <stdarg.h> /*pour va_arg*/
+#include <stdlib> /*pour Malloc et Atoi*/
+#include <string.h> /*pour strlen*/
 
 int _printf(const char *format, ...);
-endif /*MAIN_H/
+endif /*MAIN_H*/
