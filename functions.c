fbccce1c7a3b17786e940375e98d3ab5e13649e3
rpokman <roymathis74200@gmail.com>
2025-07-21 10:53:26 +0200

    update functions

--- functions.c
+++ functions.c
@@ -7,3 +7,45 @@
 *(excluding the null byte used to end output to strings)
 */
 int _printf(const char *format, ...)
+
+/**
+ * function_c - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int function_c(char c) /*function to print char c*/
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * function_s - prints strings separated by a separator
+ * @separator: string to print between strings
+ * @n: number of strings
+ */
+void function_s(const char *separator, const unsigned int n, ...)
+{
+	unsigned int a;
+	va_list args;
+	char *str;
+
+	va_start(args, n);
+
+	for (a = 0; a < n; a++)
+	{
+		str = va_arg(args, char *);
+
+		if (str == NULL)
+			printf("(nil)");
+		else
+			printf("%s", str);
+
+		if (separator != NULL && a < n - 1)
+			printf("%s", separator);
+	}
+
+	va_end(args);
+	printf("\n");
+}
