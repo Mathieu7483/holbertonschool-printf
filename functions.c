1210d94dc928e448e3943b4ae9abe7d400767111
Mathieu7483 <11436@holbertonstudents.com>
2025-07-21 13:16:47 +0200

    update functions

--- functions.c
+++ functions.c
@@ -3,7 +3,7 @@
 *_printf - The functions in the printf()
 *family produce output according to a format as described below
 *@format: format of the arguments
-*Return : the number of characters printed
+*Return: the number of characters printed
 *(excluding the null byte used to end output to strings)
 */
 int _printf(const char *format, ...)
